42cc5188e7c6f071fa2c7c3bd1e919cdc073a0ad
sunsc <584156664@qq.com>
2017-07-18 22:00:23 +0800

    just-so-so

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ sbit LSC=P2^4;
 char num=0;
 u8 DisplayData[8];
 u8 code smgduan[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
-
+u8 tem[4];
 
 void delay(u16 i)
 {
@@ -42,11 +42,15 @@ void datapros(int temp)
 		temp=tp*0.0625*100+0.5;	
 		
 	}
-	DisplayData[1] = smgduan[temp / 10000];
-	DisplayData[2] = smgduan[temp % 10000 / 1000];
-	DisplayData[3] = smgduan[temp % 1000 / 100] | 0x80;
-	DisplayData[4] = smgduan[temp % 100 / 10];
-	DisplayData[5] = smgduan[temp % 10];
+	DisplayData[1] = smgduan[temp / 10000];                         //0
+	DisplayData[2] = smgduan[temp % 10000 / 1000];                 //2
+	DisplayData[3] = smgduan[temp % 1000 / 100] | 0x80;             //9.
+	DisplayData[4] = smgduan[temp % 100 / 10];                      //9
+	DisplayData[5] = smgduan[temp % 10];                           //4
+	tem[0]=temp % 10000 / 1000+48;  //2
+	tem[1]=temp % 1000 / 100+48;    //9.
+	tem[2]=temp % 100 / 10+48;      //9
+ 	tem[3]=temp % 10+48;            //4
 }
 
 
@@ -77,18 +81,7 @@ void DigDisplay()
 	}		
 }
 
-
-void main()
-{	
-	while(1)
-	{
-		datapros(Ds18b20ReadTemp());
-		DigDisplay();
-	}		
-}
-/*#include "reg52.h"			 
-typedef unsigned int u16;	  
-typedef unsigned char u8;
+void UsartInit()
 {
 	SCON=0X50;			
 	TMOD=0X20;			
@@ -99,19 +92,35 @@ typedef unsigned char u8;
 	EA=1;						
 	TR1=1;					
 }
+
+
 void main()
 {	
 	UsartInit(); 
-	while(1);		
+	while(1)
+	{
+		datapros(Ds18b20ReadTemp());
+		DigDisplay();
+	}		
 }
+
 void Usart() interrupt 4
 {
 	u8 receiveData;
-
-	receiveData=SBUF;
+	int i;
 	RI = 0;
+	receiveData=SBUF;
+	i=receiveData;
+	switch(i)
+	{
+		case 0:receiveData=tem[0]; break;
+		case 1:receiveData=tem[1]; break;
+		case 2:receiveData='.'; break;
+		case 3:receiveData=tem[2]; break;
+		case 4:receiveData=tem[3]; break;
+	}
+
 	SBUF=receiveData;
 	while(!TI);			 
 	TI=0;						
-}*/
-
+}
